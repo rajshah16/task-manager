95005f6869317d78b790dd467c475896f8f0f245
= <=>
2022-01-30 00:00:44 +0530

    Changs in the database config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
@@ -6,15 +7,16 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT
 
+app.use(express.static('public'))
 app.use(express.json()) // This will parse the incoming json to objectid
 app.use(userRouter)
 app.use(taskRouter)
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.listen(port , ()=>{
     console.log('Server is on port ' + port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
 
 
